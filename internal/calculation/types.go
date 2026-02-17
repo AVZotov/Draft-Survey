58901afdc6b9101c45ab278612882cf7739ab1cf
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-02-17 23:49:24 +0300

    feat: add core data structures for draft survey

---
+++ internal/calculation/types.go
@@ -0,0 +1,116 @@
+package calculation
+
+import (
+	"time"
+)
+
+type FreshWaterTank struct {
+	Name     string
+	Sounding float64
+	Volume   float64
+}
+
+type BallastWaterTank struct {
+	Name     string
+	Sounding float64
+	Volume   float64
+	Density  float64
+}
+
+type Deductibles struct {
+	HFO         float64
+	MDO         float64
+	Luboil      float64
+	BilgeWater  float64
+	SewageWater float64
+	Others      float64
+	OthersName  string
+}
+
+type Marks struct {
+	FWDPort      float64
+	FWDStarboard float64
+	MIDPort      float64
+	MIDStarboard float64
+	AFTPort      float64
+	AFTStarboard float64
+}
+
+type HydrostaticRow struct {
+	Draft        float64
+	Displacement float64
+	TPC          float64
+	LCF          float64
+	LCFDirection LCFDirection
+}
+
+type MTCRow struct {
+	Draft float64
+	MTC   float64
+}
+
+type InitialDraft struct {
+	BallastWaterTanks []BallastWaterTank
+	FreshWaterTanks   []FreshWaterTank
+	Deductibles       Deductibles
+	Marks             Marks
+	ConstantDeclared  float64
+	Density           float64
+	StartedAt         time.Time
+	FinishedAt        time.Time
+	MTCRows           []MTCRow
+	HydrostaticRows   []HydrostaticRow
+}
+
+type FinalDraft struct {
+	BallastWaterTanks []BallastWaterTank
+	FreshWaterTanks   []FreshWaterTank
+	Deductibles       Deductibles
+	Marks             Marks
+	CargoDeclared     float64
+	Density           float64
+	StartedAt         time.Time
+	FinishedAt        time.Time
+	MTCRows           []MTCRow
+	HydrostaticRows   []HydrostaticRow
+}
+
+type Job struct {
+	JobNumber int
+	DSNumber  int
+	Principal string
+}
+
+type CargoOperation struct {
+	PlaceOfInspection string
+	Destination       string
+	Operation         string
+	Origin            string
+	Cargo             string
+	Packing           string
+	Port              string
+}
+type Vessel struct {
+	Name              string
+	Flag              string
+	HomePort          string
+	IMO               string
+	BuiltCountry      string
+	BuiltYear         int
+	HydrostaticTables string
+}
+
+type LCFDirection string
+
+const (
+	LCFDirectionForward LCFDirection = "F"
+	LCFDirectionAft     LCFDirection = "A"
+)
+
+type Survey struct {
+	InitialDraft   InitialDraft
+	FinalDraft     FinalDraft
+	Job            Job
+	CargoOperation CargoOperation
+	Vessel         Vessel
+}
