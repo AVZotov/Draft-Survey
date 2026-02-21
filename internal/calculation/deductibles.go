f233185330bdc10f2b6d1e1d9dcb75c3f5e5342b
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-02-21 13:52:05 +0300

    feat: add Deductibles types to calculation package

--- internal/calculation/deductibles.go
+++ internal/calculation/deductibles.go
@@ -1,1 +1,28 @@
 package calculation
+
+type OtherDeductibles struct {
+	Others     float64
+	OthersName string
+}
+
+type FreshWaterTank struct {
+	Name     string
+	Sounding float64
+	Volume   float64
+}
+
+type BallastWaterTank struct {
+	Name     string
+	Sounding float64
+	Volume   float64
+	Density  float64
+}
+
+type Deductibles struct {
+	HFO         float64
+	MDO         float64
+	LubOil      float64
+	BilgeWater  float64
+	SewageWater float64
+	OtherDeductibles
+}
