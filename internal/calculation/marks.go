645eefb6fc32ad533302e408b2f95e576b23879c
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-02-21 13:51:53 +0300

    feat: add Marks types to calculation package

--- internal/calculation/marks.go
+++ internal/calculation/marks.go
@@ -1,1 +1,22 @@
 package calculation
+
+type ReadingMethod string
+
+const (
+	ReadingMethodDirect    ReadingMethod = "direct"
+	ReadingMethodWaterline ReadingMethod = "waterline"
+)
+
+type Mark struct {
+	Value  float64
+	Method ReadingMethod
+}
+
+type Marks struct {
+	FwdPort      Mark
+	FwdStarboard Mark
+	MidPort      Mark
+	MidStarboard Mark
+	AftPort      Mark
+	AftStarboard Mark
+}
