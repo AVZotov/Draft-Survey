57b76d8f41fba42a65f4c907e9301690991db410
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-02-23 00:27:24 +0300

    feat: implement JSONStore.Get method

--- internal/storage/json_store.go
+++ internal/storage/json_store.go
@@ -15,9 +15,9 @@ type JSONStore struct {
 	TempPath string
 }
 
-func (J JSONStore) Save(id string, survey *types.Survey) error {
+func (j JSONStore) Save(id string, survey *types.Survey) error {
 	filename := id + ".json"
-	path := filepath.Join(J.Path, filename)
+	path := filepath.Join(j.Path, filename)
 	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
 		return err
@@ -33,4 +33,41 @@ func (J JSONStore) Save(id string, survey *types.Survey) error {
 		return err
 	}
 	return nil
+}
+
+func (j JSONStore) Get(id string) (*types.Survey, error) {
+	filename := id + ".json"
+	path := filepath.Join(j.Path, filename)
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer func(file *os.File) {
+		if cerr := file.Close(); cerr != nil && err == nil {
+			err = cerr
+		}
+	}(file)
+
+	decoder := json.NewDecoder(file)
+	survey := &types.Survey{}
+	if err = decoder.Decode(survey); err != nil {
+		return nil, err
+	}
+
+	return survey, nil
+}
+
+func (j JSONStore) GetBy(field SurveyField, value string) (*types.Survey, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (j JSONStore) GetAll() ([]*types.Survey, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (j JSONStore) Delete(id string) error {
+	//TODO implement me
+	panic("implement me")
 }
