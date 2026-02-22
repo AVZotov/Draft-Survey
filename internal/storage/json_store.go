b2b68bd3fda50588b2cbe5e9fe6adec363c15b32
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-02-23 00:21:25 +0300

    feat: implement JSONStore.Save method

--- internal/storage/json_store.go
+++ internal/storage/json_store.go
@@ -1,6 +1,36 @@
 package storage
 
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+
+	"github.com/AVZotov/draft-survey/internal/types"
+)
+
+var _ SurveyRepository = (*JSONStore)(nil)
+
 type JSONStore struct {
 	Path     string
 	TempPath string
+}
+
+func (J JSONStore) Save(id string, survey *types.Survey) error {
+	filename := id + ".json"
+	path := filepath.Join(J.Path, filename)
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer func(file *os.File) {
+		if cerr := file.Close(); cerr != nil && err == nil {
+			err = cerr
+		}
+	}(file)
+
+	encoder := json.NewEncoder(file)
+	if err = encoder.Encode(survey); err != nil {
+		return err
+	}
+	return nil
 }
