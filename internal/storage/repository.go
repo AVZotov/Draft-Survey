7600cc83c9da07041ec14fcffc0ada9d364dbf55
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-02-23 21:52:03 +0300

    feat: add UserRepository interface to storage package

--- internal/storage/repository.go
+++ internal/storage/repository.go
@@ -1,10 +1,18 @@
 package storage
 
-import "github.com/AVZotov/draft-survey/internal/types"
+import (
+	"github.com/AVZotov/draft-survey/internal/types"
+)
 
 type SurveyRepository interface {
 	Save(id string, survey *types.Survey) error
 	Get(id string) (*types.Survey, error)
 	GetAll() ([]*types.Survey, error)
 	Delete(id string) error
+}
+
+type UserRepository interface {
+	Save(user *types.User) error
+	Get() (*types.User, error)
+	Delete() error
 }
